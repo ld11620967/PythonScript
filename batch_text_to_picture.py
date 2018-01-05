6aad87f3df4647a9871caa9f323b8ae25532ec5a
nilin <11620967@qq.com>
2018-01-05 15:43:27 +0800

    Updata cut out text.

--- batch_text_to_picture.py
+++ batch_text_to_picture.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
  
 import os
+import re
 from PIL import Image, ImageFont, ImageDraw
 
 def rename():
@@ -11,15 +12,18 @@ def rename():
 		for file in filenames:
 			Olddir=os.path.join(dirpaths,file)#原来的文件路径
 			filename=os.path.splitext(file)[0]#文件名
-			print(filename)	
 
 			# text = u"这是一段测试文本，test 123。"
-			im = Image.new("RGB", (320, 180), (0, 255, 255))
-			dr = ImageDraw.Draw(im)
-			font = ImageFont.truetype(os.path.join("fonts", "msyh.ttf"), 20)
- 			# dr.text((30, 60), text, font=font, fill="#000000")
-			dr.text((30, 60), filename, font=font, fill="#000000")
+
+			if '_' in filename:
+				a=filename.replace(filename[0:filename.rfind('_', 1) + 1],'')	
+       			 
+				im = Image.new("RGB", (320, 180), (194, 30, 88))
+				dr = ImageDraw.Draw(im)
+				font = ImageFont.truetype(os.path.join("fonts", "msyh.ttf"), 20)
+ 				# dr.text((30, 60), text, font=font, fill="#000000")
+				dr.text((30, 70), a, font=font, fill="#FFFFFF")
  
-			# im.show()
-			im.save(filename+".png")
+				# im.show()
+				im.save(filename+".png")
 rename()
